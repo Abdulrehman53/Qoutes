c291bb88d122e3ca1ccb3ae46f222e2d0ce6d924
BillalZaman <billalzaman8@gmail.com>
2018-10-31 15:38:30 +0500

    animation creation on setting fragment on login and contact view, string setting, deleting of activities, adding of animations

--- MotivationalQuotes/app/src/main/java/com/bilalzaman/motivationalquotes/adapters/HomeAdapter.java
+++ MotivationalQuotes/app/src/main/java/com/bilalzaman/motivationalquotes/adapters/HomeAdapter.java
@@ -38,7 +38,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_home, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_quote_day, parent, false);
         context = parent.getContext();
         return new ViewHolder(view);
     }
